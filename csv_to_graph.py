bf53f540d870a4f13a289421e362542194f38b3e
zohairahmedd <151594538+zohairahmedd@users.noreply.github.com>
2024-08-13 17:21:47 -0400

    command line arguments

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -1,28 +1,50 @@
 import csv
 import matplotlib.pyplot as plt
+import argparse
 
-lst_lcornea_loaded = []
-lst_rcornea_loaded = []
+def main():
+    """
+    take command line arguments input for the path of the csv file and name of the outputted graph(s)
 
-with open('lockEBR_5000.csv', mode='r', newline='') as file:
-    reader = csv.reader(file)
-    next(reader)  
-    for row in reader:
-        lst_rcornea_loaded.append(float(row[1]))
-        lst_lcornea_loaded.append(float(row[2]))
+    arguments:
+        none
+    """
+    parser = argparse.ArgumentParser(description='processing csv files into a graph') # necessary for implementing command-line
+    parser.add_argument('--input_csv_path', '-icp', type=str, help='path to csv') # adds argument input_csv_path to the parser
+    parser.add_argument('--output_filename', '-of', type=str, help='name of graph file') # adds argument output_filename to the parser
 
-fig, ax = plt.subplots(figsize=(18, 12))
+    args = parser.parse_args() # allows us to use the arguments in the parser (args.argument_name)
 
-ax.plot(lst_lcornea_loaded, label='Left Cornea')
+    csv_to_graph(args.input_csv_path, args.output_filename) 
 
-ax.plot(lst_rcornea_loaded, label='Right Cornea', color='darkorange')
+def csv_to_graph(input_csv_path, output_filename):
 
-ax.set_xlabel('Files Processed') 
-ax.set_ylabel('Mask Area') 
-ax.legend()
+    lst_lcornea_loaded = []
+    lst_rcornea_loaded = []
 
-fig.suptitle('Cornea Mask Areas Over Files')
+    with open(input_csv_path, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader)  
+        for row in reader:
+            lst_rcornea_loaded.append(float(row[1]))
+            lst_lcornea_loaded.append(float(row[2]))
 
-plt.tight_layout()
-plt.show()
-plt.close(fig)
+    fig, ax = plt.subplots(figsize=(18, 12))
+
+    ax.plot(lst_lcornea_loaded, label='Left Cornea')
+
+    ax.plot(lst_rcornea_loaded, label='Right Cornea', color='darkorange')
+
+    ax.set_xlabel('Files Processed') 
+    ax.set_ylabel('Mask Area') 
+    ax.legend()
+
+    fig.suptitle('Cornea Mask Areas Over Files')
+
+    plt.tight_layout()
+    plt.savefig(output_filename, dpi=300)
+    plt.show()
+    plt.close(fig)
+
+if __name__ == '__main__':
+    main()
--- csv_to_superposedgraphs.py
+++ csv_to_superposedgraphs.py
@@ -1,33 +1,54 @@
 import csv
 import matplotlib.pyplot as plt
+import argparse
 
-lst_lcornea_loaded = []
-lst_rcornea_loaded = []
+def main():
+    """
+    take command line arguments input for the path of the csv file and name of the outputted graph(s)
 
-with open('lockEBR.csv', mode='r', newline='') as file:
-    reader = csv.reader(file)
-    next(reader)  
-    for row in reader:
-        lst_rcornea_loaded.append(float(row[1]))
-        lst_lcornea_loaded.append(float(row[2]))
+    arguments:
+        none
+    """
+    parser = argparse.ArgumentParser(description='processing csv files into a graph') # necessary for implementing command-line
+    parser.add_argument('--input_csv_path', '-icp', type=str, help='path to csv') # adds argument input_csv_path to the parser
+    parser.add_argument('--output_filename', '-of', type=str, help='name of graph file') # adds argument output_filename to the parser
 
-# creates subplots
-fig, axs = plt.subplots(2, 1, figsize=(18, 12), sharex=True)  # 2 rows, 1 column, shared x-axis
+    args = parser.parse_args() # allows us to use the arguments in the parser (args.argument_name)
 
-# plot lcornea on first plot
-axs[0].plot(lst_lcornea_loaded, label='Left Cornea')
-axs[0].set_ylabel('Mask Area')  
-axs[0].legend()
+    csv_to_superposed(args.input_csv_path, args.output_filename) 
 
-# plot rcornea on second plot
-axs[1].plot(lst_rcornea_loaded, label='Right Cornea', color='darkorange')
-axs[1].set_xlabel('Files Processed') 
-axs[1].set_ylabel('Mask Area') 
-axs[1].legend()
+def csv_to_superposed(input_csv_path, output_filename):
 
-fig.suptitle('Cornea Mask Areas Over Files')
+    lst_lcornea_loaded = []
+    lst_rcornea_loaded = []
 
-plt.tight_layout()
-plt.savefig('cornea_mask_areas_stacked.png', dpi=300)
-plt.show()
-plt.close(fig)
+    with open(input_csv_path, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader)  
+        for row in reader:
+            lst_rcornea_loaded.append(float(row[1]))
+            lst_lcornea_loaded.append(float(row[2]))
+
+    # creates subplots
+    fig, axs = plt.subplots(2, 1, figsize=(18, 12), sharex=True)  # 2 rows, 1 column, shared x-axis
+
+    # plot lcornea on first plot
+    axs[0].plot(lst_lcornea_loaded, label='Left Cornea')
+    axs[0].set_ylabel('Mask Area')  
+    axs[0].legend()
+
+    # plot rcornea on second plot
+    axs[1].plot(lst_rcornea_loaded, label='Right Cornea', color='darkorange')
+    axs[1].set_xlabel('Files Processed') 
+    axs[1].set_ylabel('Mask Area') 
+    axs[1].legend()
+
+    fig.suptitle('Cornea Mask Areas Over Files')
+
+    plt.tight_layout()
+    plt.savefig(output_filename, dpi=300)
+    plt.show()
+    plt.close(fig)
+
+if __name__ == '__main__':
+    main()
